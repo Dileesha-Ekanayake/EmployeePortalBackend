7015a2892e76402d8678f46ba1de5c4a24fb45a9
Dileesha-Ekanayake <113769389+Dileesha-Ekanayake@users.noreply.github.com>
2025-09-14 12:44:19 +0530

    Add missing constraints to 'CommentRequestDto'

--- EmployeePortalBackend/Dto/CommentRequestDto.cs
+++ EmployeePortalBackend/Dto/CommentRequestDto.cs
@@ -1,9 +1,16 @@
-﻿namespace EmployeePortalBackend.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeePortalBackend.Dto
 {
     public class CommentRequestDto
     {
+        [Required]
         public string Content { get; set; }
+
+        [Required]
         public int PostId { get; set; }
+
+        [Required]
         public int UserId { get; set; }
     }
 }
